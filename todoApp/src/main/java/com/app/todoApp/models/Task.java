87c33c17d997f53a7fd0ef43f09502256f5cd705
Deepash12 <142993489+Deepash12@users.noreply.github.com>
2025-06-02 15:56:57 +0530

    Task.java

--- todoApp/src/main/java/com/app/todoApp/models/Task.java
+++ todoApp/src/main/java/com/app/todoApp/models/Task.java
@@ -12,27 +12,7 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String Title;
-    private boolean Completed;
 
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setCompleted(boolean b) {
-        this.Completed = b;
-    }
-
-    public void setTitle(String title) {
-        this.Title = title;
-    }
-
-    public boolean isCompleted() {
-        return Completed;
-    }
-
+    private String title;
+    private boolean completed;
 }
